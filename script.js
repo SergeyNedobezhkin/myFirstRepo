7350a09c6d12431725a6cdce35d8caf67b8a5295
Sergey Nedobezhkin <zelenka1633@gmail.com>
2022-10-04 11:47:05 +0400

    Изменил файл lesson03 в соответствии code style

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 let title = prompt("Как называется ваш проект?");
 let screens = prompt("Какие типы экранов нужно разработать?");
 let screenPrice = +prompt("Сколько будет стоить данная работа?");
-let adaptive = true;
+let adaptive = confirm("Нужен ли адаптив на сайте?");
 let service1 = prompt("Какой дополнительный тип услуги нужен?");
 let servicePrice1 = +prompt("Сколько это будет стоить?");
 let service2 = prompt("Какой дополнительный тип услуги нужен?");
@@ -11,6 +11,12 @@ let servicePrice2 = +prompt("Сколько это будет стоить?");
 let rollback = 10;
 let fullPrice = Number(screenPrice) + Number(servicePrice1) + Number(servicePrice2);
 let servicePercentPrice = fullPrice - (fullPrice * (rollback / 100));
+
+if (adaptive == true) {
+    console.log(true);
+} else {
+    console.log(false);
+}
 
 if (fullPrice >= 30000) {
     console.log("Вам предоставляется скидка 10%");
@@ -22,23 +28,18 @@ if (fullPrice >= 30000) {
     console.log("Что то пошло не так");
 }
 
-adaptive = confirm("Нужен ли адаптив на сайте?");
-if (adaptive == true) {
-    console.log(true);
-} else {
-    console.log(false);
-}
-
 console.log(title);
 console.log(screens);
-console.log(screenPrice);
-console.log(fullPrice);
-console.log(Math.ceil(servicePercentPrice));
+console.log((screens.toLowerCase().split(", ")));
 console.log(typeof (title));
 console.log(typeof (fullPrice));
 console.log(typeof (adaptive));
 console.log(screens.length);
 console.log(screenPrice * 57 + ' RUB');
 console.log(fullPrice * 57 + ' RUB');
-console.log((screens.toLowerCase().split(", ")));
 console.log((fullPrice * rollback) / 100);
+console.log(Math.ceil(servicePercentPrice));
+console.log(screenPrice);
+console.log(fullPrice);
+
+
