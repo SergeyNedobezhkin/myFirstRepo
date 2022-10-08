825aed489fae3818adb351d325d406f565c39c58
Sergey Nedobezhkin <zelenka1633@gmail.com>
2022-10-08 09:09:12 +0400

    Выполнил задания к lesson07

--- script.js
+++ script.js
@@ -73,7 +73,6 @@ const appData = {
     logger: function () {
         for (let key in appData) {
             console.log("Ключ:" + key + " " + "Значение:" + appData[key]);
-
         }
         console.log(appData.fullPrice);
         console.log(appData.servicePercentPrice);
